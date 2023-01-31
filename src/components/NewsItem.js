79a0b05675ba1ccd208f53b3298442c29bb54d1c
manjarygupta <6672manjaryg@gmail.com>
2023-01-31 16:47:53 +0530

    news added

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,42 +3,16 @@ import React from 'react'
 export default function NewsItem(props) {
   return (
     <>
-      <div className='container'>
-            <div className='row'>
-                <div className='col-md-4'>
+      
                     <div class="card">
-                        <img src="..." class="card-img-top" alt="..."/>
+                        <img src={props.urlToImage} class="card-img-top" alt="..."/>
                         <div class="card-body">
                             <h5 class="card-title">{props.title}</h5>
                             <p class="card-text">{props.description}</p>
-                            <a href="/" class="btn btn-primary">Go somewhere</a>
+                            <a href={props.url} class="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
-                </div>
-
-                <div className='col-md-4'>
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="..."/>
-                        <div class="card-body">
-                            <h5 class="card-title">{props.title}</h5>
-                            <p class="card-text">{props.description}</p>
-                            <a href="/" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='col-md-4'>
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="..."/>
-                        <div class="card-body">
-                            <h5 class="card-title">{props.title}</h5>
-                            <p class="card-text">{props.description}</p>
-                            <a href="/" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
     </>
   )
 }
